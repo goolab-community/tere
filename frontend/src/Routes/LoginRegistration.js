f02b6df6eb6fcf40d3a03aefdda5231b414ee938
Giorgi <georgegiorgika@gmail.com>
2024-07-30 20:06:01 +0100

    feat: logreg text color

--- frontend/src/Routes/LoginRegistration.js
+++ frontend/src/Routes/LoginRegistration.js
@@ -72,9 +72,9 @@ const LogReg = () => {
   };
 
   return (
-    <div className="flex w-full mt-[--margin-top]   items-center  flex-col justify-center px-6 py-0 lg:px-8">
+    <div className="flex w-full mt-[--margin-top]   items-center  flex-col justify-center px-6 py-0 lg:px-8 text-white">
       <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-        <h2 className="text-center text-2xl font-bold leading-9 tracking-tight text-gray-900 ">
+        <h2 className="text-center text-2xl font-bold leading-9 tracking-tight ">
           {location.pathname == "/register"
             ? "Registration"
             : "Sign in to your account"}
@@ -91,7 +91,7 @@ const LogReg = () => {
             <div>
               <label
                 htmlFor="name"
-                className="block text-sm font-medium leading-6 text-gray-900"
+                className="block text-sm font-medium leading-6 "
               >
                 Your Name
               </label>
@@ -113,7 +113,7 @@ const LogReg = () => {
           <div>
             <label
               htmlFor="email"
-              className="block text-sm font-medium leading-6 text-gray-900"
+              className="block text-sm font-medium leading-6 "
             >
               Email address
             </label>
@@ -135,7 +135,7 @@ const LogReg = () => {
             <div className="flex items-center justify-between">
               <label
                 htmlFor="password"
-                className="block text-sm font-medium leading-6 text-gray-900"
+                className="block text-sm font-medium leading-6 "
               >
                 Password
               </label>
